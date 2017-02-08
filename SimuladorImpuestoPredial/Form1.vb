459bb5d12b0aa4a1fa3b2c26d025d0199b8f44db
klark <klarkking@hotmail.com>
2017-02-08 06:07:21 -0500

    item de años extraidos de la base de datos

--- SimuladorImpuestoPredial/Form1.vb
+++ SimuladorImpuestoPredial/Form1.vb
@@ -5,7 +5,11 @@ Public Class Form1
     Private ReadOnly _impuestoPredial As New ImpuestoPredial(_contexto)
     Private ReadOnly _contexto As New Contexto
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ToolStripDropDownButton1.Items.Add(_contexto)
+        For Each item In (From u In _contexto.Uits Order By u.Año Descending Select u.Año).ToList
+            ToolStripDropDownButton1.Items.Add(item)
+            ToolStripDropDownButton1.SelectedIndex = 0
+        Next
+
         ImpuestoPredialBindingSource.DataSource = _impuestoPredial
     End Sub
 End Class
